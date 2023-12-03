82d4b836daa7ee7747caa93eb51c2ac1ce272cb3
Evie-Mututa <evemututa29@gmail.com>
2023-12-03 23:52:23 +0300

    Completed assignment

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,18 @@
-# your code goes here!
+#function without the pause between each count:
+def countdown(number):
+    while number > 0:
+        print(f'{number} SECOND(S)!')
+        number -= 1
+    print("HAPPY NEW YEAR!")
+
+#function with a one second pause between each count
+import time
+
+def countdown_with_sleep(number):
+    while number > 0:
+        print(f'{number} SECOND(S)!')
+        time.sleep(1)  # Pauses for 1 second
+        number -= 1
+    print("HAPPY NEW YEAR!")
+
+
